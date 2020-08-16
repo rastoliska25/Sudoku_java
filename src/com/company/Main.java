af821832d33934516a9c3c2bc3aaa6068c6bbb68
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-08-16 11:34:23 +0200

    class names, trying scan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Backtracking filling = new Backtracking();
-
+      Backtracking filling = new Backtracking();
         File sudoku = new File("C:/Users/Rasťo//Desktop/sudoku/in/2006.txt");
         Scanner sc = new Scanner(sudoku);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
 
-        System.out.println(sc.nextLine());
+
+           for (char c:line.toCharArray()){
+               if ((c<58)&&(c>47)) System.out.print(c);
+               else if (c=='.') System.out.print(0);
+
+           }
+        }
+
 
 
      /*   System.out.println("Nevyriešené sudoku:");
@@ -24,5 +32,8 @@ public class Main {
 }
         else System.out.println("Sudoku nemá riešenie");
 */
+
+
+
     }
 }
