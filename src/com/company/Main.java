5edfbcf9e324268b4920a53d18c7af71993084ba
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-08-16 15:00:38 +0200

    scan txt - array try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,30 +12,61 @@ public class Main {
         int[][] input = new int[9][9];
 
         Backtracking filling = new Backtracking();
-        File sudoku = new File("C:/Users/Rasťo//Desktop/sudoku/in/2006.txt");
+        File sudoku = new File("C:/Users/Rasťo/Desktop/sudoku/in/2006.txt");
         Scanner sc = new Scanner(sudoku);
         int i = 0;
+        int j = 0;
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
+
             for (char c : line.toCharArray()) {
                 if ((c < 58) && (c > 47)) {
-                    System.out.print(c);
-                    i++;
-                    if (i % 9 == 0) {
-                        System.out.println();
-                    }
-                } else if (c == '.') {
-                    System.out.print(c);
-                    i++;
-                    if (i % 9 == 0) {
-                        System.out.println();
+
+                    input[i][j]=c-48;
+                    j++;
+                    if (j % 9 == 0) {
+                        j=0;
+                        i++;
+                        if (i==9) break;
                     }
                 }
+            else if (c == '.') {
+                    input[i][j]=0;
+                    j++;
+                    if (j % 9 == 0) {
+                        j= 0;
+                        i++;
+                        if (i==9) break;
+                    }
+
+                }
+
+
+            /*
+
+            for (int q = 0; q<line.length();q++){
+                if ((line.charAt(q)<58)&&(line.charAt(q)>47)){
+                    input [0][0]= (line.charAt(q))-48;
+                }
+
+
+*/
+
 
 
             }
 
         }
+
+
+
+            for (int a = 0; a<9;a++){
+                for (int b=0;b<9;b++){
+                    System.out.print(input[a][b]);
+                }
+                System.out.println();
+            }
+
 
 
 
